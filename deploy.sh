1ec45f2ee81ee8d6d352fc11e9885de9313cdaa8
Riz1402 <rizz1402@gmail.com>
2025-08-25 11:13:12 +0530

    Remove all contents from main branch

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-COMPOSE_FILE="docker-compose.yml"
-
-echo "Deploying application using Docker Compose..."
-docker-compose -f $COMPOSE_FILE up -d
-
-echo "Application deployed successfully"
